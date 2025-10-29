4bb87de43fceaa6a2733940345433bd247d632f7
chan8632 <duscks8632@gmail.com>
2025-10-29 11:33:20 +0900

    feat: ProductAll 컴포넌트 API 연동 및 상태 관리 추가

--- src/page/ProductAll.jsx
+++ src/page/ProductAll.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
+
 const ProductAll = () => {
-  return <div>상품 전체 페이지</div>;
+  const [productList, setProductList] = useState([]);
+  const getProduct = async () => {
+    const url = "http://localhost:5000/products";
+    const response = await fetch(url);
+    const data = await response.json();
+    setProductList(data);
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
+  return (
+    <div>
+      <img src="https://noona-hnm.netlify.app/track-jacket.jpeg" />
+      <div>Conscious choice</div>
+      <div>아플리케 트랙 재킷</div>
+      <div>₩99900</div>
+      <div>신제품</div>
+    </div>
+  );
 };
 
 export default ProductAll;
